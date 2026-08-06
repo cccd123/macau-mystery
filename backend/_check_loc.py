67826f21cf7f8c64b2a7e92c0ab5e218226bc517
Neflibata <oncwnuBHJKLjSH9zgKESoWtfui48@git.weixin.qq.com>
2026-08-07 02:39:39 +0800

    chore: remove temporary check scripts and product manual image

--- backend/_check_loc.py
+++
@@ -1,5 +0,0 @@
-import json
-d = json.load(open('app/locations.zh-CN.json', 'r', encoding='utf-8'))
-print(f"{len(d['items'])} locations")
-for x in d['items']:
-    print(f"  {x['id']}: {x['name']} - {x['summary'][:40]}...")
